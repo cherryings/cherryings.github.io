0e78245ddc0121380f1e0e5504a885692869518e
cherryings <2634716776@qq.com>
2023-12-16 18:16:36 +0800

    Site updated: 2023-12-16 18:16:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/13/hello-world/","2023/12/16/To-del/"];function toRandomPost(){
+var posts=["2023/12/16/To-del/","2023/12/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
