ffcbf8b365c187bbd14ab8a9566c7505c763e648
cherryings <2634716776@qq.com>
2023-12-17 17:25:56 +0800

    Site updated: 2023-12-17 17:25:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/17/BinarySearchTree搜索二叉树/","2023/12/17/多继承/","2023/12/17/模板进阶/","2023/12/17/多态/","2023/12/17/继承/"];function toRandomPost(){
+var posts=["2023/12/17/BinarySearchTree搜索二叉树/","2023/12/17/多态/","2023/12/17/多继承/","2023/12/17/继承/","2023/12/17/模板进阶/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
