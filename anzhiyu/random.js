6fe697184129bac143f49a2badd2734432f31e7e
cherryings <2634716776@qq.com>
2023-12-17 09:27:41 +0800

    Site updated: 2023-12-17 09:27:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/16/To-del/","2023/12/13/hello-world/"];function toRandomPost(){
+var posts=["2023/12/17/BinarySearchTree搜索二叉树/","2023/12/16/To-del/","2023/12/13/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
